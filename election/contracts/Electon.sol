d008e3599e4a2a70329538466cacbf2958f6e346
rrabit42 <gegiraffe@gmail.com>
2020-09-27 01:46:42 +0900

    [tutorial] Candidates list

--- election/contracts/Electon.sol
+++ election/contracts/Electon.sol
@@ -1,16 +1,26 @@
 pragma solidity 0.5.16;
 
 contract Election {
-  // Read, Write candidate
-  string public candidate;
+  // Model a Candidate
+    struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
 
-  // Constructor
-  constructor() public{
-    candidate = "Candidate 1";
+  // Read, Write Candidates
+  mapping(uint => Candidate) public candidates;
+
+  // Store Candidates Count
+  uint public candidatesCount;
+
+  function addCandidate(string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  constructor() public {
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
   }
 }
-
-// 1. 스마트 계약은 solidty version을 선언하는 pragma solidity 구문으로 시작해야 한다.
-// 2. "contract" 키워드로 계약 이름을 정의한다.
-// 3. candidate 이름을 저장할 strinㅎ 변수를 정의한다. public으로 설정하게 되면 solidity가 공짜로 해당 변수 내용을 조회할 수 있는 getter 함수를 제공해준다.
-// 4. contructor 함수는 스마트 계약이 블록체인에 배포될 때 최초에 한번 실행된다. 우리가 미리 세팅하고자 하는 변수는 migration을 통해 블록체인에 저장된다.
--- election/migrations/2_deploy_contracts.js
+++ election/migrations/2_deploy_contracts.js
@@ -6,3 +6,6 @@ var Election = artifacts.require("./Election.sol");
 module.exports = function(deployer) {
   deployer.deploy(Election);
 };
+
+// 1. 먼저 우리가 작성한 계약을 요청하여 "Election" 이라는 변수에 할당한다.
+// 2. 그 후, 이 변수를 배포된 계약의 manifest에 추가하여 우리가 migratinos를 실행할 때 우리 계약이 배포되게끔 해준다.
